004f2e3b875109d5f9b534e4482af7a460b8c4fe
ashu8tosh <ashutosh3089@gmail.com>
2019-08-26 16:17:09 +0530

    added manifests

--- src/test/java/test/pivotal/pal/trackerapi/WelcomeApiTest.java
+++ src/test/java/test/pivotal/pal/trackerapi/WelcomeApiTest.java
@@ -20,6 +20,7 @@ public class WelcomeApiTest {
 
     @Test
     public void exampleTest() {
+
         String body = this.restTemplate.getForObject("/", String.class);
         assertThat(body).isEqualTo("Hello from test");
     }
